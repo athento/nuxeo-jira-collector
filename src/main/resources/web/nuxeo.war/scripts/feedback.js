9645ff1639b16669bc9cd2604e907662c7795bf0
Angajime <tarazaky@alu.uma.es>
2015-04-16 13:09:19 +0200

    Repo init

---
+++ src/main/resources/web/nuxeo.war/scripts/feedback.js
@@ -0,0 +1,5 @@
+// Add Jira Feedback 
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://athento.atlassian.net/s/faeca45e6b8465d17c5afd85ef5579a3-T/en_UShi9tpd/64014/201/1.4.24/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=56308239";
+jQuery("head").append(s);
